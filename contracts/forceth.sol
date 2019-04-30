7537544314e28aff900b318ec1c4fddbaa751267
00jw <0x00jw@gmail.com>
2019-04-30 08:40:46 +0100

    fin

--- contracts/forceth.sol
+++ contracts/forceth.sol
@@ -1,16 +1,11 @@
 pragma solidity 0.5.1;
 
 /**
-* @title forceth
-* @notice A factory contract that creates a depositor and self destructs it to a destination,
-* forcing the ether sent to the depositor on creation to be deposited to the destination address.
-*
-* This can be used to send ether to a contract that has no payable function, or would
-* have an undesirable result should a normal ether transfer be made.
+* @title Forceth
+* @notice A tool to send ether to a contract irrespective of its default payable function
 **/
 contract Forceth {
   function sendTo(address payable destination) public payable {
-    require(destination != address(this), "Destination cannot be forceth");
     (new Depositor).value(msg.value)(destination);
   }
 }
---
+++ migrations/1_deploy_contracts.js
@@ -0,0 +1,5 @@
+var Forceth = artifacts.require("./Forceth.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Forceth);
+};
